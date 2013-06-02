03612102cd23c186220c7cf102a7395b5c53c2d2
Camilo Uribe <camilo.uribe@gmail.com>
2013-06-02 11:58:27 -0500

    borrado de archivo .nothing

--- vigilararchivoweb.sh
+++ vigilararchivoweb.sh
@@ -4,25 +4,28 @@ url=$1
 filename=$(basename $url | awk -F'.' '{print $1}')
 ext=$(basename $url | awk -F'.' '{if(NF==2) print $2; else print "html"}')
 
-cantidadArchivos=$(ls files/$filename* 2>/dev/null | wc -l )
+dir=~/files
+mkdir ~/files 2>/dev/null
+
+cantidadArchivos=$(ls $dir/$filename* 2>/dev/null | wc -l )
 if [ $cantidadArchivos -eq 0 ]; then
-    wget $url -O files/$filename$(date +"_%Y_%m_%d_%H_%M_%S")"."$ext 2>/dev/null
+    wget $url -O $dir/$filename$(date +"_%Y_%m_%d_%H_%M_%S")"."$ext 2>/dev/null
 else
-    latest=$(ls --sort=time files/$filename* 2>/dev/null | head -n 1)
+    latest=$(ls --sort=time $dir/$filename* 2>/dev/null | head -n 1)
     md5sum $latest | awk '{print $1}' > hash_latest
     
-    wget $url -O files/tmp 2>/dev/null
-    md5sum files/tmp | awk '{print $1}' > hash_downloaded
+    wget $url -O $dir/tmp 2>/dev/null
+    md5sum $dir/tmp | awk '{print $1}' > hash_downloaded
     
     diff hash_latest hash_downloaded
     iguales=$?
     
     if [ $iguales -eq 0 ]; then
 	echo "son iguales"
-	rm files/tmp
+	rm $dir/tmp
     else
 	echo "son diferentes"
-	mv files/tmp files/$filename$(date +"_%Y_%m_%d_%H_%M_%S")"."$ext
+	mv $dir/tmp $dir/$filename$(date +"_%Y_%m_%d_%H_%M_%S")"."$ext
     fi
     
     rm hash_latest hash_downloaded
